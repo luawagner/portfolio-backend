c1e381c07fd359cf5c13bda467fdc1dd373af92d
ludmila wagner <lu.wagner91@gmail.com>
2022-11-29 18:14:15 -0300

    modifico controller de proyecto, metodos update y delete

--- src/main/java/com/portfoliowagner/law/Controller/CProject.java
+++ src/main/java/com/portfoliowagner/law/Controller/CProject.java
@@ -1,4 +1,3 @@
-
 package com.portfoliowagner.law.Controller;
 
 import com.portfoliowagner.law.Dto.DtoProject;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,63 +22,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("projects")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CProject {
+
     @Autowired
     SProject sProject;
-    
+
     @GetMapping("/lista")
-    public ResponseEntity<List<Project>> list(){
+    public ResponseEntity<List<Project>> list() {
         List<Project> list = sProject.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
-   @PostMapping("/create")
-public ResponseEntity<?> create(@RequestBody DtoProject dtoPro){
-    if((dtoPro.getNombreP()).isBlank())
-        return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-    
-    Project project = new Project(dtoPro.getNombreP(), dtoPro.getDescriptionP(), dtoPro.getGithubP(), dtoPro.getImgP(), dtoPro.getUrlP());
-    sProject.save(project);
-    
-    return new ResponseEntity(new Mensaje("Proyecto agregado"), HttpStatus.OK);
-}
 
-@PutMapping("/update/(id)")
-public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoProject dtoPro){
-    if(!sProject.existsById(id))
-        return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
- if((dtoPro.getNombreP()).isBlank())
-        return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-    
- Project project = sProject.getOne(id).get();
- project.setNombreP(dtoPro.getNombreP());
- project.setDescriptionP(dtoPro.getDescriptionP());
- project.setImgP(dtoPro.getImgP());
- project.setUrlP(dtoPro.getUrlP());
- project.setGithubP(dtoPro.getGithubP());
+    @GetMapping("/detail/{id}")
 
- sProject.save(project);
- return new ResponseEntity(new Mensaje("Proyecto actualizado"), HttpStatus.OK);
+    public ResponseEntity<Project> getById(@PathVariable("id") int id) {
 
-}
-
-public ResponseEntity<?> delete(@PathVariable("id") int id){
-    if(!sProject.existsById(id))
-        return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
-sProject.delete(id);
- return new ResponseEntity(new Mensaje("Proyecto eliminado"), HttpStatus.OK);
-}
-
-@GetMapping("/detail/{id}")
-
-    public ResponseEntity<Project> getById(@PathVariable("id") int id){
-
-        if(!sProject.existsById(id))
-
+        if (!sProject.existsById(id)) {
             return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
 
         Project experiencia = sProject.getOne(id).get();
 
         return new ResponseEntity(experiencia, HttpStatus.OK);
 
     }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sProject.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
+        }
+        sProject.delete(id);
+        return new ResponseEntity(new Mensaje("Proyecto eliminado"), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody DtoProject dtoPro) {
+        if ((dtoPro.getNombreP()).isBlank()) {
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        Project project = new Project(dtoPro.getNombreP(), dtoPro.getDescriptionP(), dtoPro.getGithubP(), dtoPro.getImgP(), dtoPro.getUrlP());
+        sProject.save(project);
+
+        return new ResponseEntity(new Mensaje("Proyecto agregado"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoProject dtoPro) {
+        if (!sProject.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        Project project = sProject.getOne(id).get();
+        project.setNombreP(dtoPro.getNombreP());
+        project.setDescriptionP(dtoPro.getDescriptionP());
+        project.setImgP(dtoPro.getImgP());
+        project.setUrlP(dtoPro.getUrlP());
+        project.setGithubP(dtoPro.getGithubP());
+
+        sProject.save(project);
+        return new ResponseEntity(new Mensaje("Proyecto actualizado"), HttpStatus.OK);
+
+    }
+
+    
 
 }
